d195b5e4158bb86611fa178b895f682f3e924120
Ryan Marcotte <ryanm@iqmetrix.com>
2017-03-21 12:41:54 -0600

    Pushing new changes

---
+++ source/utility/matching/Sort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace code.utility.matching
+{
+	public static class Sort<Item>
+	{
+		public static ICompareTwoItems<Item> by_descending<ItemProperty>(
+			IGetTheValueOfAProperty<Item, ItemProperty> get_the_value_of_a_property)
+			where ItemProperty : IComparable<ItemProperty>
+		{
+			return (item1, item2) => -get_the_value_of_a_property(item1).CompareTo(get_the_value_of_a_property(item2));
+		}
+
+		public static ICompareTwoItems<Item> by_ascending<ItemProperty>(
+			IGetTheValueOfAProperty<Item, ItemProperty> get_the_value_of_a_property)
+			where ItemProperty : IComparable<ItemProperty>
+		{
+			return (item1, item2) => get_the_value_of_a_property(item1).CompareTo(get_the_value_of_a_property(item2));
+		}
+	}
+}
